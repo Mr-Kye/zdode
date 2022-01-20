64dbb01e6a68a160c270e94ae166fc3a876528a0
dalagu <dalagu@dala>
2022-01-20 20:36:46 +0900

    First commit

---
+++ src/main/java/com/example/zdode/ZdodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.zdode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zdode/ZdodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zdode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
